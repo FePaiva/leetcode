67f136fcba972fe77a4e9f285d9d04ee27d50f36
FePaiva <felippeapaiva@gmail.com>
2022-09-02 15:20:23 -0700

    Time: 94 ms (85.27%), Space: 43.2 MB (77.40%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -9,23 +9,54 @@
 //         return sSorted === tSorted
 // };
 
+// var isAnagram = function(s, t) {
+//     if(s.length !== t.length) return false
+// //define frequency tables for the strings as a hashmap.
+//     const sCount = {}
+//     const tCount = {}
+//     const N = s.length
+// //run a for loop through the characters of S and T to count how many characters
+//     for(let i = 0; i < N; i++){
+// //if the object sCount does not have a particular character, initialize the sCount to zero. Same with tCount.
+//         if(!sCount[s[i]]) sCount[s[i]] = 0
+//         if(!tCount[t[i]]) tCount[t[i]] = 0
+// //increment the count of characters 
+//         sCount[s[i]]++
+//         tCount[t[i]]++
+//     }
+//     console.log(s, ":", sCount)
+//     console.log(t, ":", tCount)
+
+// //     loop through the characters in sCount. If the frequency of certain character in sCount does not match 
+// // the frequency of that character in tCount, return false.
+//     for(let ch in sCount) {
+//         if(sCount[ch] !== tCount[ch]) return false
+//     }
+//     return true
+// };
+
+
+// sCount[ch] === tCount[ch]
+// sCount[ch] - tCount[ch] === 0 -> anagram
+
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false
 //define frequency tables for the strings as a hashmap.
-    const sCount = {}
-    const tCount = {}
+    const count = {}
     const N = s.length
 //run a for loop through the characters of S and T to count how many characters
     for(let i = 0; i < N; i++){
 //if the object sCount does not have a particular character, initialize the sCount to zero. Same with tCount.
-        if(!sCount[s[i]]) sCount[s[i]] = 0
-        if(!tCount[t[i]]) tCount[t[i]] = 0
+        if(!count[s[i]]) count[s[i]] = 0
+        if(!count[t[i]]) count[t[i]] = 0
 //increment the count of characters 
-        sCount[s[i]]++
-        tCount[t[i]]++
+        count[s[i]]++
+        count[t[i]]--
     }
-    for(let ch in sCount) {
-        if(sCount[ch] !== tCount[ch]) return false
+
+// loop through the characters. If the frequency of certain character is not Zero, return false.
+    for(let ch in count) {
+        if(count[ch] !== 0) return false
     }
     return true
 };
