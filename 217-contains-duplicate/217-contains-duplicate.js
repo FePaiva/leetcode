31a2c1313253b6248de1d6e6d1ccd3aa687d07b5
FePaiva <felippeapaiva@gmail.com>
2022-09-02 10:37:53 -0700

    Time: 272 ms (18.99%), Space: 50.7 MB (69.29%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -6,14 +6,26 @@
 // Time complexity = O(N) -> Go throug the array once.
 // Space complexity = O(N) -> Memory is taking space, and may need to go through the entire array to find solution.
 
+// var containsDuplicate = function(nums) {
+//     let memory = {}
+//     for(let i = 0; i < nums.length; i++){
+//         if(memory[nums[i]] === undefined){
+//             memory[nums[i]] = i
+//         } else {
+//             return true;
+//         }
+//     }
+//     return false;
+// };
+
+// Time complexity = O(N) -> Go throug the array once.
+// Space complexity = O(NlogN) -> Sort the array so the duplicates are next to each other.
 var containsDuplicate = function(nums) {
-    let memory = {}
+    nums.sort((a,b) => {return b-a});
     for(let i = 0; i < nums.length; i++){
-        if(memory[nums[i]] === undefined){
-            memory[nums[i]] = i
-        } else {
-            return true;
+        if(i > 0 && nums[i-1] === nums[i]){
+            return true
         }
     }
-    return false;
-};
+    return false
+}
