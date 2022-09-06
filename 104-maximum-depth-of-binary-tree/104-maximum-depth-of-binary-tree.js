a7b3efb967efb6baa98f5ddf0fac5a30c6feeb25
FePaiva <felippeapaiva@gmail.com>
2022-09-06 10:44:45 -0700

    Time: 137 ms (7.61%), Space: 44.7 MB (91.54%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -10,13 +10,23 @@
  * @param {TreeNode} root
  * @return {number}
  */
+// MAIN IDEA FOR SOLUTION: ACCESS EACH VALUE IN CORRECT ORDER PER LEVEL AND ADD THEM TO NEW ARRAY. COUNT HOW MANY TIMES A NEW ARRAY WAS CREATED.
+// if (!root) return [] -> if root is null, return empty array.
+// let queue = [root]  -> initialize a queue modeled using an array
+// while (queue.length != 0) -> queue contains node elements, we will run a while loop.
+// let node = queue.pop() -> within the loop, we want to remove the first element from the top of the queue. This will be the current node.
+// if (node.left/right) queue.unshift(node.left/right) -> check if the node has any left or right children and add those to the end of the queue.
+// let subarr = [] -> define a subarray for the level, this will reset before each new level. 
+// for (let i = 0; i < n; i++) -> inside the while loop (which runs till the queue is empty), we define a for-loop. The for-loop will run for each of the elements in the queue at the end of the last level. This means we will increment “i” until it reaches the queue length.
+// let node = queue.pop() -> inside the for-loop, remove a node from the top of the queue.
+// subarr.push(node.val) -> add the removed node to the level subarray.
+// let maximumDepth = 0 -> initialize a counter to count how many times nodes from each level were added to subarr.
+// maximumDepth++ -> increment by 1 each time nodes were added to subarray.
+// return Math.max(maximumDepth) -> return the largest amount of maximumDepth, which will be the last level of the binary tree.
 
-// if root is null, return null.
-// 
 
 var maxDepth = function(root) {
-    if (!root) return []
-    let result = []      
+    if (!root) return []      
     let queue = [root] 
     let maximumDepth = 0
     while (queue.length != 0) {
@@ -26,11 +36,10 @@ var maxDepth = function(root) {
             let node = queue.pop()
             subarr.push(node.val)
             if (node.left) queue.unshift(node.left)
-            if (node.right) queue.unshift(node.right)
-            
+            if (node.right) queue.unshift(node.right)      
         }
-        result.push(subarr)
         maximumDepth++
     }
     return Math.max(maximumDepth)
 }
+
