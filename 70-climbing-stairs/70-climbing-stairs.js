ed02f1087b3aa729adb0cfc14bb3aff1acfa5158
FePaiva <felippeapaiva@gmail.com>
2022-09-05 17:26:38 -0700

    Time: 94 ms (33.92%), Space: 41.9 MB (42.66%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -2,14 +2,37 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-    let output = [1,2]
+// var climbStairs = function(n) {
+//     let output = [1,2]
     
-    if(n < 3) {
-        return output[n - 1]
+//     if(n < 3) {
+//         return output[n - 1]
+//     }
+//     for (let i = 2; i < n; i++){
+//         output.push(output[i - 2] + output[i - 1])
+//     }
+//     return output[n - 1]
+// };
+
+// Time complexity = O(N) -> bc we have to iterate once.
+// Space complexity = O(N) -> which is the size of the memoization array.
+var climbStairs = function(n) {
+ const memo = [1,1,2];
+    
+    for(let i = 3; i <= n; i++){
+        memo[i] = memo[i-1] + memo[i-2];
     }
-    for (let i = 2; i < n; i++){
-        output.push(output[i - 2] + output[i - 1])
-    }
-    return output[n - 1]
-};
+    return memo[n]
+}
+
+// in order to climb N steps, we add the results of n-1 and n-2 steps.
+// n=1 -> 1 -> 1 way to climb
+// n=2 -> 11,2 -> 2 ways to climb
+// n=3 -> 111,21,12 -> 3 ways to climb (result of n-2 + n-1)
+// n=4 -> 1111, 22, 211, 112, 121 -> 5 ways to climb (result of n-3 + n-2)
+// n=5 -> 11111, 221, 122, 212, 2111, 1211, 1121, 1112 -> 8 ways to climb (result of n-4 + n-3)
+
+// memo[i] = memo[i-1] + memo[i-2] 
+// inside the loop, add the first 2 values to ge the value.
+//  const memo = [1,1,2] -> the initial value is 1,1,2. Do not need to care about the first "1" because 
+// N is supposed to be greater than 1.
