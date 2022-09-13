0f7b87cb0b83233903e108840da651e1e64df2fc
FePaiva <felippeapaiva@gmail.com>
2022-09-13 18:28:40 -0400

    Time: 103 ms (28.24%), Space: 42.3 MB (33.20%) - LeetHub

--- 27-remove-element/27-remove-element.js
+++ 27-remove-element/27-remove-element.js
@@ -3,15 +3,23 @@
  * @param {number} val
  * @return {number}
  */
+// var removeElement = function(nums, val) {
+//    let i = 0
+//     for(let j = 0; j < nums.length; j++){
+//       // console.log(nums[j])
+//       // Swap numbers if current number is not equal to val
+//       if(nums[j] !== val){
+//           nums[i] = nums[j];
+//           // Index where numbers that are equal to val begin in the array
+//           i++
+//       }
+//     } return i
+// };
+
 var removeElement = function(nums, val) {
-   let i = 0
-    for(let j = 0; j < nums.length; j++){
-      // console.log(nums[j])
-      // Swap numbers if current number is not equal to val
-      if(nums[j] !== val){
-          nums[i] = nums[j];
-          // Index where numbers that are equal to val begin in the array
-          i++
-      }
-    } return i
-};
+    for(let i = nums.length - 1; i >= 0; i--){
+        if(nums[i] === val) 
+            nums.splice(i, 1);
+    }   
+    return nums.length;
+}
