f661f7bb8cb183e523cac7b36cf2bf3e568e3d46
FePaiva <felippeapaiva@gmail.com>
2022-09-20 18:45:26 -0400

    Time: 165 ms (46.72%), Space: 44.1 MB (99.20%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -2,13 +2,18 @@
  * @param {string} s
  * @return {number}
  */
+
+// if (s.length === 0) return -1 -> if empty string, return -1.
+// for( let i = 0; i < s.length; i++) -> loop through the string.
+// if(s.indexOf(s[i]) === s.lastIndexOf(s[i])) {return i} -> if the first index of s[i] is equal to the last index of s[i], it means that this is a unique character, so need to return i.
+// return -1 -> return -1 in case the first and last index of s[i] are not equals.
+
 var firstUniqChar = function(s) {
     if (s.length === 0) return -1;
 
    for( let i = 0; i < s.length; i++) {
-       let letter = s[i]
 
-       if(s.indexOf(letter) === s.lastIndexOf(letter)) {
+       if(s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
            return i
        }
    }
