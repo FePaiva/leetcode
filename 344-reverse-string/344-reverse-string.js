71eef11f5dfcc39b5038f30a37e712299537f748
FePaiva <felippeapaiva@gmail.com>
2022-09-20 19:05:50 -0400

    Time: 165 ms (27.85%), Space: 48.4 MB (99.13%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -6,6 +6,14 @@
 //     return s.reverse()
 // };
 
+
+// using 2 pointers.
+// set first and last pointers
+// while first <= last pointer, create temporary and assign to the first pointer (s[first])
+// swap first and last pointers
+// assign the last pointer to temp
+// keep looping through the other elements until it is over.
+
 var reverseString = function(s) {
     let first = 0;
     let last = s.length - 1;
