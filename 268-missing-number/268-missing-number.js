d8b9c0b9fb81ca5c9b4bc18b24dc2ff01c64c700
FePaiva <felippeapaiva@gmail.com>
2022-09-21 12:54:12 -0400

    Time: 110 ms (54.54%), Space: 44.7 MB (38.08%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -2,13 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
-    let sorted = nums.sort((a,b) => a - b)
+// var missingNumber = function(nums) {
+//     let sorted = nums.sort((a,b) => a - b)
     
-    for(let i = 0; i < nums.length; i++){
-        // console.log("numsi", nums[i])
-        if(nums[i] !== i) return i;
-        // console.log("i", i)
-    }
-    return nums[nums.length - 1] + 1;
-};
+//     for(let i = 0; i < nums.length; i++){
+//         // console.log("numsi", nums[i])
+//         if(nums[i] !== i) return i;
+//         // console.log("i", i)
+//     }
+//     return nums[nums.length - 1] + 1;
+// };
+
+
+var missingNumber = function(nums) {
+    
+    let nSum = nums.reduce((previousValue, currentValue) => previousValue + currentValue);
+        // console.log("nSum", nSum);
+    
+    let gSum = (nums.length * (nums.length + 1)) / 2;
+    // console.log("gSum", gSum)
+    
+    return gSum - nSum
+    
+}
