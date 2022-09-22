a03f9b720b4458aa1e370bb467710c9bd8580095
FePaiva <felippeapaiva@gmail.com>
2022-09-21 21:14:45 -0400

    Time: 151 ms (50.87%), Space: 49.9 MB (73.63%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.js
@@ -2,9 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-// var findDuplicate = function(nums) {
-//        return nums.find((e,i)=> nums.indexOf(e)!=i)
-// };
+
 
 // nums = [1,3,4,2,2]
 // index : indeces -> 0 : 1 , 1 : 3 , 2 : 4 , 3 : 2 , 4 : 2 -> see this array as a graph
@@ -20,6 +18,9 @@
 // fast and slow are in the correct postion (if(fast === slow)), so now need to increment slow (could be fast as well) along with pointer, and when slow and pointer intercept, the node where they are coming from is the answer (the index).
 // while(pointer !== slow){pointer = nums[pointer];slow = nums[slow]; -> while pointer is not intercepting slow, need to increment pointer and slow.
 // return pointer; -> when pointer and slow intercept, return the value that was right before they meet (slow or pointer).
+
+// Time complexity: O(N) -> becasue iterating though the array once.
+// Space complexity: O(1) -> I am creating space (fast / slow), but that is not increasing / decrrasing relative to the input.
 
 var findDuplicate = function(nums) {
     let fast = 0;
