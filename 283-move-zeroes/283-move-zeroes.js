f9449dca32335d2c981463170ef3ede3019ed3f0
FePaiva <felippeapaiva@gmail.com>
2022-09-22 19:27:03 -0400

    Time: 153 ms (43.38%), Space: 46.6 MB (58.31%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -2,20 +2,37 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
+// var moveZeroes = function(nums) {
     
-    let index = 0
+//     let index = 0
+    
+//     for(let i = 0; i < nums.length; i++){
+//         console.log(nums[i])
+//         if(nums[i] !== 0){
+//              nums[index] = nums[i]
+//              index +=1
+//         }
+//     }
+    
+//     for(let i = index; i < nums.length; i++){
+//         nums[i] = 0
+//         }
+    
+// };
+
+
+var moveZeroes = function(nums) {
+    let prev = 0
     
     for(let i = 0; i < nums.length; i++){
-        console.log(nums[i])
-        if(nums[i] !== 0){
-             nums[index] = nums[i]
-             index +=1
-        }
+        if (nums[i] !== 0) {
+            let temp = nums[prev]
+            nums[prev] = nums[i]
+            nums[i] = temp
+            prev++
+        }   
     }
     
-    for(let i = index; i < nums.length; i++){
-        nums[i] = 0
-        }
     
-};
+}
+
