762d5705d5096ee9568d9b547f36bc1e3fc9efe3
FePaiva <felippeapaiva@gmail.com>
2022-12-02 20:27:37 -0500

    Time: 95 ms (61.97%), Space: 43.2 MB (62.54%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,16 +4,20 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    const nums3 = []
-    for(let i = 0; i < nums1.length; i++){
-        console.log(nums1[i], nums2[i])
-        for(let j = 0; j < nums2.length; j++){
-            if(nums1[i] === nums2[j]){
-                nums3.push(nums1[i])
-            }
-        }
-    }
-    const uniqueNums = [...new Set(nums3)]
-    return uniqueNums
+    // const nums3 = []
+    // for(let i = 0; i < nums1.length; i++){
+    //     console.log(nums1[i], nums2[i])
+    //     for(let j = 0; j < nums2.length; j++){
+    //         if(nums1[i] === nums2[j]){
+    //             nums3.push(nums1[i])
+    //         }
+    //     }
+    // }
+    // const uniqueNums = [...new Set(nums3)]
+    // return uniqueNums
+    
+  const set1 = new Set(nums1)
+  const set2 = new Set(nums2)
+  return [...set1].filter(num => set2.has(num))
     
 };
