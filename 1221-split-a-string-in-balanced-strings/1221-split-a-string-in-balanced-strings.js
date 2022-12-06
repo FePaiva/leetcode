d6de0e54dc1e9c7320628821370efe083f367e78
FePaiva <felippeapaiva@gmail.com>
2022-12-05 20:49:43 -0500

    Time: 209 ms (5.07%), Space: 49.2 MB (5.25%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -2,6 +2,26 @@
  * @param {string} s
  * @return {number}
  */
+
+//   s = "R L R R L L R L R L"
+//        + - + + - - + - + -
+//         0   +2 -2   0   0
+//count    0     0     0   0
+//ANSWER   1.    1.    1.  1 = 4
+
+//    s = "R L R R R L L R L L"
+//         - + - - - + + - + +
+//          0    -3   +2 -1 +2
+//COUNT       0           0
+//ANSWER.     1           1 = 2
+
+//      s = "L L L L R R R R"
+//           + + + + - - - -
+//              +4      -4
+//COUNT              0
+//ANSWER             1 
+
+
 var balancedStringSplit = function(s) {
     let counter = 0
     let answer = 0
