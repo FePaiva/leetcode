eb4390469b85de2c1165aa65dd254f2eb5ee295c
FePaiva <felippeapaiva@gmail.com>
2022-12-12 20:49:26 -0500

    Time: 117 ms (49.86%), Space: 44.6 MB (21.97%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -9,25 +9,42 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// var deleteDuplicates = function(head) {
+    
+//   let current = head;
+//   let prev = null;
+
+//   while (current != null) {
+//     // check if current element is the same as the previous element
+//     if (prev != null && current.val === prev.val) {
+//       // remove the current element from the list
+//       prev.next = current.next;
+//     } else {
+//       // update the previous element
+//       prev = current;
+//     }
+
+//     // move to the next element in the list
+//     current = current.next;
+//   }
+
+//   return head;
+
+// };
+
 var deleteDuplicates = function(head) {
     
-  let current = head;
-  let prev = null;
-
-  while (current != null) {
-    // check if current element is the same as the previous element
-    if (prev != null && current.val === prev.val) {
-      // remove the current element from the list
-      prev.next = current.next;
-    } else {
-      // update the previous element
-      prev = current;
+    let current = head;
+    
+    while(current && current.next){
+        if(current.val === current.next.val){
+            current.next = current.next.next
+        } else {
+            current = current.next
+        }
     }
+    return head;
+}
 
-    // move to the next element in the list
-    current = current.next;
-  }
 
-  return head;
 
-};
