7bcbf7fefafbbb98237a345b89331a16a4210491
FePaiva <felippeapaiva@gmail.com>
2023-01-03 17:52:55 -0800

    Time: 98 ms (31.52%), Space: 42.4 MB (12.23%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -14,3 +14,8 @@ var numberOfSteps = function(num) {
     }
     return steps
 };
+
+
+// The time complexity -> O(n), where n is the number of steps required to reach 0. This is because the loop will run for a maximum of n iterations, and each iteration has a constant time complexity (O(1)).
+
+// The space complexity -> O(1), because the only variables that are being used are num and steps, which have a constant size regardless of the input size.
